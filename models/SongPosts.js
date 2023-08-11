8db14289e042971b197e7deab7896c297cebaf7b
Corey Gulledge <coreygulledge35@gmail.com>
2023-08-10 20:38:26 -0400

    added songposts and comments and created the model for them

---
+++ models/SongPosts.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("./config/connection");
+
+class SongPosts extends model {}
+
+SongPosts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    song_link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post: {
+      type: DataTypes.STRING(140),
+      allownull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "songposts",
+  }
+);
+
+module.exports = SongPosts;
