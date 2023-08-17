0c906517b2adacfde2e894563e5e9593b7080ba7
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-08-16 22:43:05 -0500

    added reply btn, fixed code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
-const userRoutes = require('./UserRoutes');
-const SongPostRoutes = require('./SongpostRoutes');
+
+
+
 const router = require("express").Router();
 const userRoutes = require("./UserRoutes");
 const songPostRoutes = require("./SongPostRoutes");
 const commentsRoutes = require("./CommentsRoutes");
 
 router.use('/users', userRoutes);
-router.use('/projects', SongPostRoutes);
+router.use('/projects', songPostRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,4 +8,13 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 })
+router.get('/songposts', async (req, res) => {
+  try {
+    res.render('songposts', { 
+        logged_in: req.session.logged_in 
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 module.exports = router;
