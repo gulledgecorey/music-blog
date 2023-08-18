094a2a6219be8880957b384eb3597a85fdd571d7
unknown <kaseybrown28@icloud.com>
2023-08-17 21:38:39 -0400

    added a newPost handlebars, a newPost js file, updated the homeRoutes js file, updated the main.handlebars, updated styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,25 @@
-const router = require('express').Router();
-router.get('/', async (req, res) => {
+const withAuth = require("../utils/auth");
+const { User, SongPosts, Comments } = require("../models");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
   try {
-    res.render('homepage', { 
-        logged_in: req.session.logged_in 
-      });
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
+
+router.get("/new-post", withAuth, async (req, res) => {
+  try {
+    res.render("newPost", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,19 @@
+const newPost = () => {
+  document.location.replace("/new-post");
+};
+document.querySelector(".new-post").addEventListener("click", newPost);
+
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to log out.');
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to log out.");
+  }
+};
+
+document.querySelector("#logout").addEventListener("click", logout);
