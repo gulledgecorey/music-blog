c17d2452e746791b20c42c5db9ce7a962ab38172
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-08-17 22:45:35 -0400

    comments update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,4 +17,29 @@ router.get('/songposts', async (req, res) => {
     res.status(500).json(err);
   }
 })
+
+router.get('/comments', async (req, res) => {
+  try {
+    res.render('comments', { 
+        logged_in: req.session.logged_in 
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+//added this for comments 
+router.post('/comments', async (req, res) => {
+  try {
+    const { commentText } = req.body; 
+    const newComment = await saveCommentToDatabase(commentText);
+
+    res.status(201).json(newComment); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
 module.exports = router;
